61da7d9347c889f94c718d42318664636e526215
Yongjin Cho <yongjin.cho@gmail.com>
2023-01-03 10:37:13 +0900

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,10 +16,16 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- General
-  use 'morhetz/gruvbox' -- Theme
+  use 'morhetz/gruvbox' -- Colorscheme
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+
+  -- Editor
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'nvim-lualine/lualine.nvim'
+  use 'numToStr/Comment.nvim'
+
+  -- Languages
+  use 'fatih/vim-go'
 
   -- Git
   use 'tpope/vim-fugitive'
@@ -42,6 +48,8 @@ require('packer').startup(function(use)
       'j-hui/fidget.nvim',
     },
   }
+
+  -- Autocompletion
   use {
     'hrsh7th/nvim-cmp', -- Autocompletion plugin
     requires = {
@@ -59,6 +67,10 @@ require('packer').startup(function(use)
   end
 end)
 
+if packer_bootstrap then
+  return
+end
+
 
 -- General options (`:help vim.o`)
 vim.o.number = true
@@ -72,8 +84,13 @@ vim.o.smartcase = true
 
 
 -- General shorcuts
+local nmap = function(lhs, rhs, opts)
+  opts = opts or {}
+  vim.keymap.set('n', lhs, rhs, opts)
+end
+
 vim.g.mapleader = ' '
-vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', { silent=true })
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 
 -- lualine
@@ -82,6 +99,11 @@ require('lualine').setup {
     icons_enabled = false,
     theme = 'gruvbox_dark',
   },
+  sections = {
+    lualine_c = {
+      { 'filename', path = 1 },
+    }
+  }
 }
 
 
@@ -94,6 +116,10 @@ require('indent_blankline').setup {
 }
 
 
+-- Enable Comment.nvim
+require('Comment').setup()
+
+
 -- gitsigns (`:help gitsigns.txt`)
 require('gitsigns').setup {
   --current_line_blame = true,
@@ -101,58 +127,66 @@ require('gitsigns').setup {
 
 
 -- Telescope
--- `brew install ripgrep` for live_grep
-pcall(require('telescope').load_extension, 'fzf')
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local telescope = require('telescope.builtin')
+pcall(require('telescope').load_extension, 'fzf') -- `brew install ripgrep` for live_grep
+nmap('<leader><space>', function ()
+  telescope.buffers{
+    sort_mru = true,
+    initial_mode = 'normal',
+    path_display = function(_, path)
+      local tail = require("telescope.utils").path_tail(path)
+      if tail == path then
+        return string.format(path)
+      else
+        local head = string.sub(path, 0, string.len(path)-string.len(tail)-1)
+        return string.format("%s (%s)", tail, head)
+      end
+    end,
+  }
+end)
+nmap('<leader>ff', telescope.find_files)
+nmap('<leader>fg', telescope.live_grep)
+nmap('<leader>fh', telescope.help_tags)
 
 
 -- LSP settings.
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+nmap('[d', vim.diagnostic.goto_prev)
+nmap(']d', vim.diagnostic.goto_next)
+nmap('<leader>e', vim.diagnostic.open_float)
+nmap('<leader>q', vim.diagnostic.setloclist)
 
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
+  local lmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
     end
-
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  lmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  lmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  lmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  lmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  lmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
+  lmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  lmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  lmap('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+  lmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  lmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  lmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  lmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  lmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  lmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function() vim.lsp.buf.format() end, { desc = 'Format current buffer with LSP' })
 end
 
 -- Enable the following language servers
@@ -169,7 +203,6 @@ local servers = {
   tsserver = {},
   -- clangd = {},
   -- rust_analyzer = {},
-
   sumneko_lua = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -219,11 +252,12 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort,
     ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+      --behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
@@ -250,3 +284,30 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+
+-- Language specific settings
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = "go",
+  callback = function()
+    vim.bo.tabstop = 4
+  end
+})
+
+
+-- Load old vim plugins
+local ensure_plug = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/autoload/plug.vim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system{'curl', '-fLo', install_path, '--create-dirs',
+              'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'}
+  end
+end
+ensure_plug()
+
+vim.cmd[[
+call plug#begin('~/.local/share/nvim/plugged')
+Plug 'jlanzarotta/bufexplorer'
+call plug#end()
+]]
